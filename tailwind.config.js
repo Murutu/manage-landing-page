9def774f8266255dbde82705023581d31f04f25f
Murutu <murutupeter@gmail.com>
2023-05-12 10:48:07 +0300

    chore: made changes to the  customization & configuration of the app

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,17 +8,18 @@ module.exports = {
       lg: "976px",
       xl: "1440px"
     },
-    colors: {
-      brightRed: 'hsl(12, 88%, 59%)',
-      brightRedLight: 'hsl(12, 88%, 69%)',
-      brightRedSupLight: 'hsl(12, 88%, 95%)',
-      darkBlue: 'hsl(228, 39%, 23%)',
-      darkGrayishBlue: 'hsl(227, 12%, 61%)',
-      veryDarkBlue: 'hsl(233, 12%, 13%)',
-      veryPaleRed: 'hsl(13, 100%, 96%)',
-      veryLightGray: 'hsl(0, 0%, 98%)'
+    extend: {
+      colors: {
+        brightRed: 'hsl(12, 88%, 59%)',
+        brightRedLight: 'hsl(12, 88%, 69%)',
+        brightRedSupLight: 'hsl(12, 88%, 95%)',
+        darkBlue: 'hsl(228, 39%, 23%)',
+        darkGrayishBlue: 'hsl(227, 12%, 61%)',
+        veryDarkBlue: 'hsl(233, 12%, 13%)',
+        veryPaleRed: 'hsl(13, 100%, 96%)',
+        veryLightGray: 'hsl(0, 0%, 98%)'
+      }
     },
-    extend: {},
   },
   plugins: [],
 }
@@ -29,8 +30,7 @@ This is the theme section in my tailwind.config.js file
 Where you define your projects color palette, type scale, fonts, breakpoints,
 border radius values & more.
 
-If you'd like to extend default spacing scale, 
-you can do so using the `theme.extend.spacing` 
-section of your `tailwind config.js` file.
+If you want to include all of the default colors in Tailwind, 
+you have to include the new colors in an "extends" bracket so it won't overwrite everything else.
 */
 
